3ca68a998602cdad05d70f02f83f1d148e922592
ts-yuta.oki <ts-yuta.oki@rakuten.com>
2017-12-18 09:28:40 +0900

    Change schema and config sample file

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS test_send_histories (
-  id           INT(11)    NOT NULL AUTO_INCREMENT,
-  job_id       INT(11)    UNIQUE,
-  bundle_type  TINYINT(4) NOT NULL,
-  bundle_id    INT(11)    NOT NULL,
-  status       TINYINT(4) NOT NULL,
-  info         TEXT           NULL,
+  id          INT(11)    NOT NULL AUTO_INCREMENT,
+  job_id      INT(11)    UNIQUE,
+  bundle_type TINYINT(4) NOT NULL,
+  bundle_id   INT(11)    NOT NULL,
+  status      TINYINT(4) NOT NULL,
+  info        MEDIUMTEXT NULL,
   PRIMARY KEY (id),
   KEY idx_job_id (job_id) USING BTREE,
   KEY idx_bundle (bundle_type, bundle_id) USING BTREE
