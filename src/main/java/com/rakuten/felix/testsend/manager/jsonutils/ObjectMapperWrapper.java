513b218849577be2c41f8b42aa3dcf40d1cc9452
Inoue, Shigeki b | DSD <ts-shigeki.b.inoue@rakuten.com>
2018-04-27 12:23:43 +0900

    modify build.gradle for spring boot version up

--- src/main/java/com/rakuten/felix/testsend/manager/jsonutils/ObjectMapperWrapper.java
+++ src/main/java/com/rakuten/felix/testsend/manager/jsonutils/ObjectMapperWrapper.java
@@ -1,6 +1,7 @@
 package com.rakuten.felix.testsend.manager.jsonutils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.stereotype.Service;
 
