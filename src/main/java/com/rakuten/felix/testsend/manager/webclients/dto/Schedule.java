af27c833e5fb6b0e5589c467cea8ca5c2a05b799
ts-yuta.oki <ts-yuta.oki@rakuten.com>
2018-05-29 16:45:47 +0900

    Add reserveDate column in mail job class and fix validation annotation

--- src/main/java/com/rakuten/felix/testsend/manager/webclients/dto/Schedule.java
+++ src/main/java/com/rakuten/felix/testsend/manager/webclients/dto/Schedule.java
@@ -1,14 +1,23 @@
 package com.rakuten.felix.testsend.manager.webclients.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.rakuten.felix.testsend.manager.serde.UnixTimeStampDeserializer;
+import com.rakuten.felix.testsend.manager.serde.UnixTimeStampSerializer;
 import lombok.Value;
 
 import javax.validation.constraints.NotNull;
+import java.time.ZonedDateTime;
 import java.util.List;
 
 @Value
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Schedule {
+    @JsonDeserialize(contentUsing = UnixTimeStampDeserializer.class)
+    @JsonSerialize(contentUsing = UnixTimeStampSerializer.class)
+    @NotNull
+    ZonedDateTime reserveDate;
     @NotNull
     List<Subject> subjects;
     @NotNull
