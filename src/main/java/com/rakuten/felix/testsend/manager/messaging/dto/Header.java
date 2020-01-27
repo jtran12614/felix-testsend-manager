650f57c7dc80d2f3feccadee4d5ef520de2457b8
Yuya Kusaka <yuya.a.kusaka@rakuten.com>
2020-01-27 16:42:23 +0900

    deleted namespace

--- src/main/java/com/rakuten/felix/testsend/manager/messaging/dto/Header.java
+++ src/main/java/com/rakuten/felix/testsend/manager/messaging/dto/Header.java
@@ -13,13 +13,6 @@ public class Header extends HashMap<String, Object> {
     private static final String TEST_ID_KEY = "TEST_ID";
     private static final String REPLY_DESTINATION_KEY = "REPLY_DESTINATION";
 
-    private static final String NAMESPACE = "namespace";
-    private static final String NAMESPACE_LINE = "LINE";
-
-    public Header() {
-        this.putIfAbsent(NAMESPACE, NAMESPACE_LINE);
-    }
-
     public String getLogId() {
         return (String) this.get(LOG_ID_KEY);
     }
