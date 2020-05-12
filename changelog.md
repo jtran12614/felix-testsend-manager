8dd3b096f27d33325d5ec2a5c2a9f9a88b685af3
Quyen Phan <quyenvuminh.phan@rakuten.com>
2020-05-12 14:28:40 +0900

    FELIX-3111 Add Hikari configuration

--- changelog.md
+++ changelog.md
@@ -4,6 +4,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/)
 and this project adheres to [Semantic Versioning](http://semver.org/).
 
+## 1.1.6 - 2020-05-12
+### Added
+- Add Hikari configuration
+
 ## 1.1.5 - 2020-01-14
 ### Added
 - Added controller for line
--- src/main/java/com/rakuten/felix/testsend/manager/Application.java
+++ src/main/java/com/rakuten/felix/testsend/manager/Application.java
@@ -1,17 +1,16 @@
 package com.rakuten.felix.testsend.manager;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.ApplicationPidFileWriter;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.retry.annotation.EnableRetry;
 
 @EnableRetry
 @SpringBootApplication
 @Slf4j
-public class Application implements ApplicationListener<ContextRefreshedEvent> {
+public class Application implements CommandLineRunner {
     private final VersionInfo versionInfo;
 
     public Application(VersionInfo versionInfo) {
@@ -31,7 +30,7 @@ public class Application implements ApplicationListener<ContextRefreshedEvent> {
     }
 
     @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
+    public void run(String... args) {
         log.info("Application started: version={}, commit={}, build start time={}", versionInfo.getVersion(), versionInfo.getCommit(), versionInfo.getBuildStartTimeAsFormattedString());
     }
 }
